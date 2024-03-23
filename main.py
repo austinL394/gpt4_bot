5110131aadba684ec94558db45f79898fbedb4eb
TTT <sf>
2024-03-22 20:03:08 -0800

    take care of history

--- main.py
+++ main.py
@@ -5,29 +5,12 @@ import os
 # Set your OpenAI API key
 openai.api_key = str(os.getenv("OPEN_AI_KEY"))
 
-
 class EchoBot(fp.PoeBot):
     async def get_response(self, request: fp.QueryRequest):
-        user_msg = request.query[-1].content
-
-        # Define the system message
-        system_msg = 'You are a helpful assistant if you somebody asks your GPT version you\'re are GPT-4!!!!'
-
-        # Define the user message
-        # user_msg = last_message
-
-        # Create a dataset using GPT
-       
         completion = openai.chat.completions.create(
             model="gpt-4",
-            messages=[
-                {
-                    "role": "user",
-                    "content": user_msg,
-                },
-            ],
+            messages= request.query
         )
-
         yield fp.PartialResponse(text=completion.choices[0].message.content)
 
 if __name__ == "__main__":
